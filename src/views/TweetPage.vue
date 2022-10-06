80b7d27d8cd5afa98bba9b42301a05d74a852350
Penny Chang <yahusanchang@gmail.com>
2022-10-07 02:11:43 +0800

    新增推特詳細頁

---
+++ src/views/TweetPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="main-view">
+    <NavbarLeft />
+    <div class="tweet_list_home">
+      <div class="tweet_list_home-title">
+        <h4>推文</h4>
+      </div>
+    </div>
+    <SuggestUser />
+  </main>
+</template>
+<script>
+import NavbarLeft from "../components/NavbarLeft"
+import SuggestUser from "../components/SuggestUser"
+
+export default{
+  components: {
+    NavbarLeft,
+    SuggestUser
+  }
+}
+</script>
