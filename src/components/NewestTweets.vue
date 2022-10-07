0d1f1fb8d0e8dc7a9c063c35464a63c793283001
Penny Chang <yahusanchang@gmail.com>
2022-10-07 21:18:05 +0800

    TweetReply串假資料

--- src/components/NewestTweets.vue
+++ src/components/NewestTweets.vue
@@ -12,7 +12,7 @@
         <div class="comment_wrap_body--content mb-3"><router-link :to="{name: 'tweet', params: { id: tweet.id }}">{{tweet.content}}</router-link></div>
         <div class="comment_wrap_footer d-flex">
           <div class="comment_wrap_footer--comments-num d-flex mr-10">
-            <div class="icon comment my-auto" @click.stop.prevent = "openModal"></div>
+            <div class="icon comment my-auto" @click.stop.prevent = "[openModal(), getTweet(tweet.id)]"></div>
             <span class="number-wrap">{{tweet.commentsLength}}</span>
           </div>
           <div class="comment_wrap_footer--liked-num d-flex">
@@ -21,8 +21,8 @@
           </div>
         </div>
       </div>
+      <TweetReply :is-show="isShow" :tweet="currentTweet" @close-modal="closeModal"/>
     </div>
-    <TweetReply :is-show="isShow" @close-modal="closeModal"/>
   </div>
 </template>
 
@@ -35,7 +35,19 @@ import { accountFilter } from './../utils/mixins'
 export default{
   data (){
     return{
-      isShow: false
+      isShow: false,
+      currentTweet: {
+        id: 0,
+        content: '',
+        video: null,
+        createAt: '',
+        account: '',
+        name: '',
+        likedLength: 0,
+        commentsLength: 0,
+        isFavorite: false,
+        avatar: null
+      }
     }
   },
   components: {
@@ -66,6 +78,13 @@ export default{
     },
     closeModal (show){
       return this.isShow = show
+    },
+    getTweet (tweetId) {
+      const target = this.tweets.filter((tweet) => tweet.id === tweetId )
+      this.currentTweet = {
+        ...this.currentTweet,
+        ...target[0]
+      }
     }
   }
 }
