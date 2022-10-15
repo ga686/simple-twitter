cd139caaa54208c0645ed10395eefd2a3b7b901c
ChingG0 <www.karta2428620@gmail.com>
2022-10-15 11:03:09 +0800

    feat: remove app style

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,4 @@
 
 <style lang="scss">
 @import './assets/scss/basic.scss';
-
-#app {
-  width: 576px;
-  padding: 0 19px;
-}
-
-@media screen and (min-width: 992px) and (max-width: 1199px) {
-  #app {
-    width: 100%;
-    padding: 100px 16px;
-  }
-}
-
-@media screen and (min-width: 1200px) {
-  #app {
-    width: 100%;
-    padding: 100px 130px;
-  }
-}
 </style>
