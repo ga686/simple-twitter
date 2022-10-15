58de4a18e88b56df5217b92fbc5b4d3d79a0affa
ChingG0 <www.karta2428620@gmail.com>
2022-10-15 13:18:43 +0800

    feat: modify userEdit form name toLowerCase

--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -123,6 +123,7 @@ export default {
         this.isProcessing = true
         const form = e.target
         const formData = new FormData(form)
+        formData.set('name', formData.get('name').toLowerCase());
         const { data } = await usersAPI.updateProfile(this.initUser.id, formData)
         if(data.status === "error"){
           throw new Error(data.message)
